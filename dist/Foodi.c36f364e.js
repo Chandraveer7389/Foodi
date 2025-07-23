6846df055ad4cb881e3d16b998ab75c2c3491519
Chandraveer <chandraveer7389@gmail.com>
2025-07-23 21:43:58 +0530

    Added minor changes

---
+++ dist/Foodi.c36f364e.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello REACT");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=Foodi.c36f364e.js.map
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello REACT");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (<h1>Hey how are u React</h1>)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
