f74ecfc0e3e32f7c960ae3c26ea606987ebd96ee
Chandraveer <chandraveer7389@gmail.com>
2025-07-29 23:12:55 +0530

    Made some changes

--- dist/Foodi.c36f364e.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "Hello REACT");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=Foodi.c36f364e.js.map
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const Header = () => {
+    return (<div className="header">
+        <div className="logo-container">
+            <img
+            className="logo" src="./logo-img/food-logo.jpg"
+            style={{ width: "200px", height: "auto", border: "2px solid red" }}/>
+        </div>
+        <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>Cart</li>
+            <li>Contact Us</li>
+            <li>About</li>
+        </ul>
+        </div>
+    </div>)
+}
 
-const heading = (<h1>Hey how are u React</h1>)
 
+const AppLayout = () => {
+    return (<div className="app">
+        <Header/>
+    </div>)
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout/>);
