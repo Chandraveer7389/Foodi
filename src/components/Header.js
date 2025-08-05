b47187b3a081bd139b81738c78b95c5a7cd4d5b5
Chandraveer <chandraveer7389@gmail.com>
2025-08-05 22:47:26 +0530

    Added a login btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 const foodlogo = new URL("../../assets/food-logo.jpg",import.meta.url);
-
+import { useState } from "react";
 const Header = () => {
+    const [btnState,setBtnState] = useState("Login")
     return (<div className="header">
         <div className="logo-container">
             <img
@@ -13,6 +14,9 @@ const Header = () => {
             <li>Cart</li>
             <li>Contact Us</li>
             <li>About</li>
+            <button className="btn-login" onClick={()=>{ 
+                (btnState === "Login")? setBtnState("Logout"):setBtnState("Login");
+            }}>{btnState}</button>
         </ul>
         </div>
     </div>)
