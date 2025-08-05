3504e55b79ce21343a3e3c8cd0039afc9edf9efb
Chandraveer <chandraveer7389@gmail.com>
2025-08-05 16:58:52 +0530

    config driven ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,42 +2,37 @@ import ResCard from "./ResCard";
 import { useEffect, useState } from "react";
 
 const Body = () => {
-  let [listItems, setListItems] = useState([
-    {
-      id: "1",
-      resname: "Meghna Foods",
-      cuisine: "Biryani, North Indian, Asian",
-      rating: 4.4,
-    },
-    {
-      id: "2",
-      resname: "KFC",
-      cuisine: "Burgers, Fast Food",
-      rating: 4.1,
-    },
-    {
-      id: "3",
-      resname: "Domino's Pizza",
-      cuisine: "Pizza, Italian, Pasta",
-      rating: 3.9,
-    },
-  ]);
-  useEffect(()=>{console.log("use effect"),[]})
-  console.log("hey")
+  let [listItems, setListItems] = useState([]);
+  const fetchData = async () => {
+    const data = await fetch("https://raw.githubusercontent.com/namastedev/namaste-react/refs/heads/main/swiggy-api");
+    const json = await data.json();
+    console.log(json);
+    setListItems(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+  };
+
+  useEffect(() => {
+    fetchData()
+  },[]);
+
   return (
     <div>
       <div className="btn-container">
-        <button className="btn" onClick={() =>{
+        <button
+          className="btn"
+          onClick={() => {
             const topRated = listItems.filter((items) => {
-                return items.rating > 4.0;
-            })
-            setListItems(topRated)
-        }}>Top Rated restaurant</button>
+              return items.rating > 4.0;
+            });
+            setListItems(topRated);
+          }}
+        >
+          Top Rated restaurant
+        </button>
       </div>
       <div className="search-container"> Search</div>
       <div className="res-container">
         {listItems.map((restaurant) => (
-            <ResCard key={restaurant.id} resInfo={restaurant} />
+          <ResCard key={restaurant.info.id} resInfo={restaurant.info} />
         ))}
       </div>
     </div>
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,9 +1,10 @@
 const ResCard = ({resInfo}) => {
     return (<div className="res-card">
-        <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/cbcaa9d8-e337-420a-8d76-cb250ec96b4c_741531.jpg" alt="Food" />
-        <h2>{resInfo.resname}</h2>
-        <p>{resInfo.cuisine}</p>
-        <p>Rating: {resInfo.rating}</p>
+        <img className="res-img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resInfo.cloudinaryImageId} alt="Food" />
+        <h2>{resInfo.name}</h2>
+        <p>{resInfo.locality}</p>
+        <p>{resInfo.cuisines.join(" ,")}</p>
+        <p>Rating: {resInfo.avgRating}</p>
     </div>)
 }
 export default ResCard;
