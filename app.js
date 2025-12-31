c8f554d31931fac8235a715a56afe2022bc2b74a
Chandraveer <chandraveer7389@gmail.com>
2025-12-31 23:26:19 +0530

    Created header component

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Title = <h1>Hey, my name is</h1>
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={require("./logo.png")} alt="food-logo"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
 const App = () => {
   return (
-    <div>
-        {Title}
-      <h1>Chandraveer</h1>
+    <div className="app">
+      <Header/>
     </div>
   );
 };
