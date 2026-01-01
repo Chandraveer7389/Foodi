9ee3aec3d03c067cc481ecd30d4b75330f795a1d
Chandraveer <chandraveer7389@gmail.com>
2026-01-01 12:27:13 +0530

    Created Restaurant card and body

--- app.js
+++ app.js
@@ -18,12 +18,31 @@ const Header = () => {
     </div>
   )
 }
+const Card = () => {
+  return (
+    <div className="card">
+      <img className="card-logo" src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg" alt="image-of-food"/>
+      <h3 className="card-header">Meghna food</h3>
+      <p className="card-cuisines">roti, daal and biryani</p>
+      <p className="card-rating">4.5</p>
+      <p className="card-delivery-time">34 min</p>
 
-
+    </div>
+  )
+}
+const Body = () => {
+  return (<div className="body">
+    <div className="search container">Search</div>
+    <div className="res-container">
+      <Card/>
+    </div>
+  </div>)
+}
 const App = () => {
   return (
     <div className="app">
       <Header/>
+      <Body/>
     </div>
   );
 };
